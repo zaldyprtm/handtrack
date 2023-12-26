f504a79dc10920a3170517bd7a4f5a712c9391f3
LordVoldemort <lordvoldemort@localhost.localdomain>
2023-12-26 18:09:37 +0700

    add

---
+++ hand.py
@@ -0,0 +1,14 @@
+#Tracking Tangan Webcam
+
+import cv2
+from cvzone import HandTrackingModule
+
+
+cap = cv2.VideoCapture(0)
+detector = HandTrackingModule.HandDetector()
+
+while(True):
+    success,img=cap.read()
+    detector.findHands(img)
+    cv2.imshow('Image', img)
+    cv2.waitKey(1)
